338436a11be0ab1b29fc077153e11e3558f4d694
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-18 00:01:52 +0800

    ca

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import todo
-from backend.database import Base, engine
+from routers import todo  # or from backend.routers import todo if outside backend
+from database import Base, engine
 
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173/todoappreact-django",  # Vite default dev server
-    "https://gwayni.github.io/todo_fastapi/",
-    # Add your deployed frontend URL
+    "http://localhost:5173",
+    "https://gwayni.github.io/todo_fastapi/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,            # list of allowed origins
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],              # allow all HTTP methods
-    allow_headers=["*"],              # allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
 
-backend.include_router(todo.router, prefix="/todos", tags=["ToDos"])
+app.include_router(todo.router, prefix="/todos", tags=["ToDos"])
