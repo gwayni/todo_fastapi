c15a50f3d4ae1f7b7751f8f785a911e95cb605f8
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-18 19:30:51 +0800

    waht

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
-class ToDo(Base):
+class Todo(Base):  # Changed class name from ToDo to Todo
     __tablename__ = "todos"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
