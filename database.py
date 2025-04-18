596917c0c4c79a18c14c6c7e0e23bf8482210ac2
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-18 17:22:46 +0800

    sda

--- database.py
+++ database.py
@@ -1,10 +1,17 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://todo_fastapi_arm5_user:rnp974ZFrjJC0cNCAsQdczTSDbbpiKMG@dpg-d00hu7adbo4c73930r60-a/todo_fastapi_arm5"  # Change to PostgreSQL URI if deploying
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
