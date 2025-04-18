b1c7c4f7be3b01841a991f6b932542465d96e6f1
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-18 17:09:13 +0800

    sch

--- schemas.py
+++ schemas.py
@@ -8,7 +8,7 @@ class TodoUpdate(BaseModel):
     title: Optional[str] = None
     completed: Optional[bool] = None
 
-class Todo(BaseModel):
+class ToDoResponse(BaseModel):
     id: int
     title: str
     completed: bool
