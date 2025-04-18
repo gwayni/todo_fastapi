156e452d5dfead23ca44a74aaa975cd3c5b20cb1
Gwayne Rithzel Colaste <160402153+gwayni@users.noreply.github.com>
2025-04-18 19:03:10 +0800

    Update database.py

--- database.py
+++ database.py
@@ -3,15 +3,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+# Get the PostgreSQL connection string from environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(
-        DATABASE_URL, connect_args={"check_same_thread": False}
-    )
-else:
-    engine = create_engine(DATABASE_URL)
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
 
+# Create a sessionmaker to interact with the database
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for the models
 Base = declarative_base()
